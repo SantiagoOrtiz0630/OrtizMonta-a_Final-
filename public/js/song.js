17abd14fc312eb31343d7a6711b81f26ee2f9d76
Santiago <san.or.gue@gmail.com>
2019-01-11 00:22:20 -0500

    Updated song view

--- public/js/song.js
+++ public/js/song.js
@@ -10,15 +10,19 @@ var visualizer = function (p) {
     var height = canvas.clientHeight;
     var x = width / 2;
     var y = height / 2;
-    var radio = width / 5;
+    var radio = width / 3.2;
+    var anchoLineas = width / 320;
     var song;
     var ancho = width / 100;
     var numLineas = 100;
     var tipe = true;
     var amplitude;
-    var anchoLineas = width / 500;
 
     var special = false;
+
+    let audioSpect = [];
+    let audioSpectDos = [];
+    let spectrum;
 
     p.preload = function () {
         p.soundFormats('mp3', 'ogg');
@@ -39,14 +43,39 @@ var visualizer = function (p) {
         p.noStroke();
         p.angleMode(p.DEGREES);
         song.loop();
+
+        spectrum = fft.waveform();
+
+        audioSpect = new Array(8);
+        for (var i = 0; i < audioSpect.length; i++) {
+            audioSpect[i] = new Array(10);
+        }
+
+        for (let a = 0; a < audioSpect.length; a++) {
+            for (let b = 0; b < audioSpect[a].length; b++) {
+                audioSpect[a][b] = false;
+            }
+        }
+
+        audioSpectDos = new Array(8);
+        for (var i = 0; i < audioSpectDos.length; i++) {
+            audioSpectDos[i] = new Array(10);
+        }
+
+        for (let a = 0; a < audioSpectDos.length; a++) {
+            for (let b = 0; b < audioSpectDos[a].length; b++) {
+                audioSpectDos[a][b] = false;
+            }
+        }
+
+        p.frameRate(60);
     };
 
     p.draw = function () {
         p.background(20);
-        let spectrum = fft.analyze();
+        spectrum = fft.analyze();
 
         var amp = (20 * amplitude.getLevel());
-
 
         if (tipe) {
             for (let i = 0; i < numLineas; i++) {
@@ -71,7 +100,72 @@ var visualizer = function (p) {
                 } catch (e) {}
             }
         } else {
-            p.fill(250, 250, 250, 120);
+
+            //EQ VIEW
+
+            let marX = width / 53.3333;
+            let marY = height / 53.3333;
+
+            let tamX = width / 9.6269;
+            let tamY = height / 12.5;
+
+            p.rectMode(p.CORNER);
+            for (let x = 0; x < audioSpectDos.length; x++) {
+                let i = 0;
+                for (let y = 0; y < audioSpectDos[0].length; y++) {
+                    if (audioSpectDos[x][y]) {
+                        let op = 33 - (i * 3);
+                        p.fill(240, 240, 250, op);
+                        p.rect((width - tamX) - (marX + (x * (tamX + marX))), (height - (marY + tamY)) - (y * (tamY + marY)), tamX,
+                            tamY);
+                        p.noFill();
+                        p.fill(255);
+                        p.noFill();
+                        i++;
+
+                        audioSpectDos[x][y] = false;
+                    }
+                }
+            }
+
+            p.rectMode(p.CENTER);
+
+            p.noFill();
+
+            p.rectMode(p.CORNER);
+            for (let x = 0; x < audioSpect.length; x++) {
+                let i = 0;
+                for (let y = 0; y < audioSpect[0].length; y++) {
+                    if (audioSpect[x][y]) {
+                        let op = 55 - (i * 5);
+                        if (special == false) {
+                            p.fill(22, 136, 230, 80);
+                        } else {
+                            p.fill(243, 72, 113, 90);
+                        }
+                        p.rect((marX + (x * (tamX + marX))), (height - (marY + tamY)) - (y * (tamY + marY)), tamX,
+                            tamY);
+                        p.noFill();
+                        p.fill(255);
+                        p.noFill();
+                        i++;
+
+                        audioSpect[x][y] = false;
+                    }
+                }
+            }
+
+            p.rectMode(p.CENTER);
+
+            p.noFill();
+
+            p.eqCalculate();
+
+            //EQ VIEW
+
+            //LINEAR VIEW
+
+            /* 
             for (let i = 0; i < numLineas; i++) {
                 try {
                     var newAncho = (width - 20) / 100;
@@ -91,9 +185,83 @@ var visualizer = function (p) {
                     p.pop();
                 } catch (e) {}
             }
+            */
         }
         p.noFill();
     };
+
+    p.eqCalculate = function () {
+        let num = 50;
+
+        //
+        let mov = 25;
+
+        for (let i = mov; i < 1000; i += num) {
+            let val = 0;
+
+            for (let j = 0; j < num; j++) {
+                let valor = Math.abs(spectrum[i + j]);
+                val += (valor * 1);
+            }
+
+            let prom = val / num;
+
+            let index = (i - mov) / num;
+
+            if (index < audioSpect.length) {
+                let n = parseInt(prom / 10) - 8;
+
+                if (n < 0) {
+                    n = 0;
+                }
+
+                if (n > audioSpect[0].length) {
+                    n = 10;
+                }
+
+                for (let j = 0; j < n; j++) {
+                    audioSpect[index][j] = true;
+                }
+            } else {
+                break;
+            }
+        }
+        //
+        //
+        let mov2 = mov + (num * audioSpect.length);
+
+        for (let i = mov2; i < 1000; i += num) {
+            let val = 0;
+
+            for (let j = 0; j < num; j++) {
+                let valor = Math.abs(spectrum[i + j]);
+                val += (valor * 1);
+            }
+
+            let prom = val / num;
+
+            let index = (i - mov2) / num;
+
+            if (index < audioSpectDos.length) {
+                let n = parseInt(prom / 10);
+
+                if (n < 0) {
+                    n = 0;
+                }
+
+                if (n > audioSpectDos[0].length) {
+                    n = 10;
+                }
+
+                for (let j = 0; j < n; j++) {
+                    audioSpectDos[index][j] = true;
+                }
+            } else {
+                break;
+            }
+        }
+        //
+    }
 
     p.changeSong = function (i) {
         if (song != null) {
@@ -128,8 +296,8 @@ var visualizer = function (p) {
     p.windowResized = function () {
         width = canvas.clientWidth;
         height = canvas.clientHeight;
-        anchoLineas = width / 500;
-        radio = width / 5;
+        anchoLineas = width / 320;
+        radio = width / 3.2;
         ancho = width / 100;
         x = width / 2;
         y = height / 2;
@@ -140,9 +308,7 @@ var visualizer = function (p) {
 
 var myp5 = new p5(visualizer, canvas);
 
-
 //menu
-
 
 var btnPlay = document.getElementById('play');
 btnPlay.addEventListener('click', myp5.changePlay);
